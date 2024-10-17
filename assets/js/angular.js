b4506a0e23a966ab4192674884fc557a95ef872b
arena-abhishek <apdesign0001@gmail.com>
2024-10-17 14:55:03 +0530

    add

--- assets/js/angular.js
+++ assets/js/angular.js
@@ -4,12 +4,22 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.directive("cartIcon", function () {
   return {
     link: function (scope, element) {
-      console.log("Cart icon clicked");
-      element.on("click", function (event) {
-        console.log("Cart icon clicked again");
+      // Initialize showCart on the scope
+      scope.showCart = false;
+
+      // Function to handle cart icon click
+      function handleClick(event) {
         event.preventDefault();
-        scope.showCart = true;
-        scope.$apply();
+        scope.showCart = !scope.showCart; // Toggle the cart visibility
+        scope.$apply(); // Apply changes to the scope
+      }
+
+      // Attach the event listener to the element
+      element.on("click", handleClick);
+
+      // Unbind the event listener when the scope is destroyed
+      scope.$on("$destroy", function () {
+        element.off("click", handleClick);
       });
     },
   };
@@ -17,16 +27,14 @@ app.directive("cartIcon", function () {
 
 app.config(function ($routeProvider) {
   $routeProvider
-    .when("/", {
-      templateUrl: "index copy.html",
-      controller: "HomeController",
+    .when("/home", {
+      templateUrl: "index.html",
     })
     .when("/contact", {
       templateUrl: "contact.html",
     })
     .when("/about", {
       templateUrl: "about.html",
-      controller: "AboutController",
     })
     .when("/signup", {
       templateUrl: "signup.html",
@@ -39,6 +47,7 @@ app.config(function ($routeProvider) {
     });
 });
 
+// Controllers
 app.controller("HomeController", function ($scope) {
   $scope.message = "Welcome to the home page!";
 });
@@ -47,16 +56,17 @@ app.controller("AboutController", function ($scope) {
   $scope.message = "Welcome to the about page!";
 });
 
-app.run(function ($rootScope, $location) {
-  $rootScope.$on("$routeChangeStart", function (event, next, current) {
+app.run(function ($rootScope) {
+  $rootScope.$on("$routeChangeStart", function (event, next) {
     console.log("Route changing to: ", next.templateUrl);
   });
 });
 
-var myApp = angular.module("myApp", []);
+/* var myApp = angular.module("myApp", []);
 myApp.controller("CardController", function ($scope) {
   $scope.cartCount = 0;
   $scope.addToCart = function () {
     $scope.cartCount++;
   };
 });
+ */
